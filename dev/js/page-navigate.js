afb5db8514afc986e20e5afba30f1317f8d4c3b8
Vitalij Khomenko <homenkovit@gmail.com>
2016-04-22 14:01:39 +0300

    Add smooth navigate for small screen (with scroll and without FullPageScroll.js)

--- dev/js/page-navigate.js
+++ dev/js/page-navigate.js
@@ -23,6 +23,14 @@ var StartFullpage = function() {
 if ( windowWidth>1000 && windowHeight>620) {
 	
 	StartFullpage();
+} else {
+  $('.nav__link').on('click', function(e) {
+    e.preventDefault();
+
+    showSection($(this).attr('href'), true);
+  });
+
+  showSection(window.location.hash, false);
 }
 
 
@@ -74,4 +82,40 @@ SwitchFullpagePlugin.init();
 //         return xScroll    
 // });
 
+  
+
 });
+
+$(window).scroll(function() {
+  checkSection();
+});
+
+function showSection(section, isAnimate) {
+  var direction = section.replace(/#/, ''),
+      reqSection = $('.section').filter('[data-section="' + direction + '"]'),
+      reqSectionPos = reqSection.offset().top;
+
+  if (isAnimate) {
+    $('body, html').animate({scrollTop: reqSectionPos}, 300);
+  } else {
+    $('body, html').scrollTop(reqSectionPos);
+  }
+}
+
+function checkSection() {
+  $('.section').each(function() {
+    var $this = $(this),
+        topEdge = $this.offset().top - 200,
+        bottomEdge = topEdge + $this.height(),
+        wScroll = $(window).scrollTop();
+    if (topEdge < wScroll && bottomEdge > wScroll) {
+      var currentId = $this.data('section'),
+          reqLink = $('.nav__link').filter('[href="#' + currentId + '"]');
+      
+      reqLink.closest('.nav__item').addClass('active')
+        .siblings().removeClass('active');
+
+      window.location.hash = currentId;
+    }
+  });
+};
